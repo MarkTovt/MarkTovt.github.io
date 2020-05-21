bf920400598b5a2ced74336f684e1d75f61f7a5f
MarkTovt <marktovt@gmail.com>
2020-05-21 22:44:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
+'use strict'
+
 const navbar = document.querySelector("#navbar");
 const sticky = navbar.offsetTop;
-const requestURL = 'https://MarkTovt.github.io/blob/master/techs.json';
+const techCard = document.querySelector(".techCard");
+const requestURL = 'https://MarkTovt.github.io/techs.json';
 const request = new XMLHttpRequest();
 request.open('GET', requestURL);
 request.responseType = 'json';
 request.send();
 request.onload = function() {
   const techs = request.response;
-
-console.log(techs);
+  createTechCards(techs);
+  console.log(techs[0]['name']);
 }
 window.onscroll = function() {
   if (window.pageYOffset >= sticky) {
@@ -16,77 +19,18 @@ window.onscroll = function() {
   } else {
     navbar.classList.remove("sticky");
   }
-};
-
-function createTechCards(technologies){
-  const { name, image, description } = technologies;
-  const card = `
-    <img src="${image}"></img>
-    <h3>${name}</h3>
-    <p>${description}</p>
-  `;
-  techCard.innerHTML( card);
 }
 
-function init(){
-  
-    console.log('hello');
-  
+let increment = 0;
+
+function createTechCards(jsonObj){
+  const myH1 = document.createElement('h3');
+  const techImage = document.createElement('img');
+  const techDescription = document.createElement('p');
+  myH1.textContent = jsonObj[increment]['name'];
+  techImage.src = jsonObj[increment]['image'];
+  techDescription.textContent = jsonObj[increment]['description'];
+  techCard.appendChild(myH1);
+  techCard.appendChild(techImage);
+  techCard.appendChild(techDescription);
 }
-init();
-
-/* Select all links with hashes
-$('a[href*="#"]')
-  // Remove links that don't actually link to anything
-  .not('[href="#"]')
-  .not('[href="#0"]')
-  .click(function(event) {
-    // On-page links
-    if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-      && 
-      location.hostname == this.hostname
-    ) {
-      // Figure out element to scroll to
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      // Does a scroll target exist?
-      if (target.length) {
-        // Only prevent default if animation is actually gonna happen
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000, function() {
-          // Callback after animation
-          // Must change focus!
-          var $target = $(target);
-          $target.focus();
-          if ($target.is(":focus")) { // Checking if the target was focused
-            return false;
-          } else {
-            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-            $target.focus(); // Set focus again
-          };
-        });
-      }
-    }
-  });
-
-var i = 0;
-
-var testDiv = document.querySelectorAll("div.techKnoledge p");
-
-function displayContent(){  
-  testDiv[i].classList.add("transform");
-  i = (i==testDiv.length-1) ? 0 : i+1;
-  testDiv[i].classList.remove("transform");
-}
-
-function displayContentReverse(){
-  testDiv[i].classList.add("transform");
-  i = (i==0) ? testDiv.length-1 : i-1;
-  testDiv[i].classList.remove("transform");
-}
-
-document.getElementById("lButton").addEventListener("click", displayContentReverse);
-document.getElementById("rButton").addEventListener("click", displayContent);*/
